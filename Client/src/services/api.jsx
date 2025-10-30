9315882398304dc2bee256598686bd50829bb661
Tapas kundu <tapaskundu3762@gmail.com>
2025-10-31 01:16:38 +0530

    write VITE_API_BASE in frontend env

--- Client/src/services/api.jsx
+++ Client/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://imagesearch-via-unsplash-api.onrender.com';
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 const api = axios.create({
   baseURL: API_BASE,
